e30aea2ee97f601b164e6d86f3a5ae3c623d6931
peterstromstedt <110103285+peterstromstedt@users.noreply.github.com>
2023-11-19 15:28:15 +0100

    version 1.0

---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps(['income', 'expenses']);
+</script>
+
+<template>
+    <div class="inc-exp-container">
+        <div>
+            <h4>Income</h4>
+            <p id="money-plus" class="money plus">+${{ income }}</p>
+        </div>
+        <div>
+            <h4>Expense</h4>
+            <p id="money-minus" class="money minus">${{ expenses }}</p>
+        </div>
+    </div>
+</template>
